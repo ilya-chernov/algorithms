67b5067d351241668b20136bf7532f82759d5f1e
Ilya Chernov <i.chernov@hotmail.com>
2024-04-21 10:18:41 +0200

    Реализовал наивный и прогрессивный подход к решению Фибоначчи

--- src/Fibonacci.java
+++ src/Fibonacci.java
@@ -1,8 +1,8 @@
+import java.util.HashMap;
 import java.util.Scanner;
 
 public class Fibonacci {
     public static int FibonacciNaive(int number) {
-        // наивный подход. Сложность n^2 - жутко не оптимальный вариант
         if (number <= 2) {
             return 1;
         } else {
@@ -10,13 +10,49 @@ public class Fibonacci {
         }
     }
 
-    public static void main(String[] args) {
-        System.out.println("Нахождение числа Фибоначчи.");
-        System.out.print("Введите число с клавиатуры: ");
-        Scanner console = new Scanner(System.in); // инициализируем объект console типа scanner
-        int number = console.nextInt();
+    static HashMap<Integer, Integer> hashMap = new HashMap<>();
+    public static int FibonacciSmart(int n) {
+        if (n <= 2) {
+            return 1;
+        }
 
-        String response = String.format("f(%s) = ", number);
-        System.out.println(response + FibonacciNaive(number));
+        if (hashMap.get(n) != null) {
+            return hashMap.get(n);
+        } else {
+            int value = FibonacciSmart(n - 1) + FibonacciSmart(n - 2);
+            hashMap.put(n, value);
+            return hashMap.get(n);
+        }
+    }
+
+
+    public static void main(String[] args) {
+        Scanner console = new Scanner(System.in); // инициализируем объект console типа scanner
+
+        System.out.println("Нахождение числа Фибоначчи. Выберите алгоритм");
+        System.out.println("1 - Наивный. Дохнет при числах покрупнее");
+        System.out.println("2 - С кешированием. Вычисления сохраняются в HashMap");
+        System.out.print("Введите число с клавиатуры: ");
+
+        int option = console.nextInt();
+
+        if (option == 1) {
+            System.out.print("Выбран 1 алгоритм. Введите число с клавиатуры: ");
+            int number = console.nextInt();
+            String response = String.format("f(%s) = ", number);
+            System.out.println(response + FibonacciNaive(number));
+        }
+
+        if (option == 2) {
+            System.out.print("Выбран 2 алгоритм. Введите число с клавиатуры: ");
+            int number = console.nextInt();
+            String response = String.format("f(%s) = ", number);
+            System.out.println(response + FibonacciSmart(number));
+        }
+
+        if ((option != 1) && (option != 2)) {
+            System.out.println("Неверный вариант меню.");
+        }
+
     }
 }
